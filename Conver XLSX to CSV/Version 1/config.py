6024a9d51ec74a251ab09ec64e8ba1183ca5f38c
Ducmanh28 <95476360+Ducmanh28@users.noreply.github.com>
2024-11-25 19:05:41 +0700

    cập nhật công việc

---
+++ Conver XLSX to CSV/Version 1/config.py
@@ -0,0 +1,5 @@
+file_path = 'device_convert_input.xlsx'
+
+sheet_name='Input'
+
+output_file_path = 'output_file1.csv'
---
+++ Conver XLSX to CSV/Version 2/config.py
@@ -0,0 +1,7 @@
+file_path = 'sample_input_for_conver.xlsx'
+
+sheet_name = 'Input'
+
+output_file_path = 'output_file2.csv'
+
+site = 'VNPT NTL'
